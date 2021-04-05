e71b67c125c3c2cc83c96f4302a0007305786c8d
roshaniayu <asti.roshani@gmail.com>
2021-04-05 17:38:01 +0700

    Initial Commit

---
+++ NailIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NailIt
+//
+//  Created by Roshani Ayu Pranasti on 05/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
