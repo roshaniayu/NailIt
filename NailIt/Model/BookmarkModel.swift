2253ea9d51b8fd7041bf2ad79b546bc52127d6e1
roshaniayu <asti.roshani@gmail.com>
2021-04-08 22:04:40 +0700

    merge with dicky

---
+++ NailIt/Model/BookmarkModel.swift
@@ -0,0 +1,15 @@
+//
+//  BookmarkModel.swift
+//  NailIt
+//
+//  Created by Roshani Ayu Pranasti on 07/04/21.
+//
+
+import Foundation
+
+struct BookmarkModel {
+    var id_question: Int = 0
+    var id_bookmark: Int = 0
+    var question: String = ""
+    var notes: String = ""
+}
