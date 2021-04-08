c44817c68b8c42690a2a8c662a5da23907382f94
muhsanmaulana <muhsan.maulana@gmail.com>
2021-04-08 19:19:10 +0800

    try the view

---
+++ NailIt/YourAnswerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  YourAnswerViewController.swift
+//  NailIt
+//
+//  Created by Muhsan Maulana on 08/04/21.
+//
+
+import UIKit
+import MobileCoreServices
+
+
+
+
+class YourAnswerViewController: UIViewController {
+    
+    @IBOutlet weak var recordVideo: UIButton!
+    
+    @IBOutlet weak var viewVideo: UIButton!
+
+
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+
+        
+        
+        
+}
+    
+    @IBAction func recordVideoAction(_ sender: Any) {
+
+       
+        
+
+    }
+    
+    
+    @IBAction func videoVideoAction(_ sender: Any) {
+    }
+    
+   
+    
+    
+}
+
